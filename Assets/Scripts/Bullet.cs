e5d84e79e67c7bb6fe3bd495312cbcc6419b96ed
sajad maghsoodi <maghsoudi@unboundgamestudio.com>
2021-09-16 18:40:08 +0430

    Episode 2

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private float _force;
+
+    private void Start()
+    {
+        _rigidbody.AddForce(transform.right * _force);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,24 +5,71 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    [SerializeField] private Rigidbody2D _rigidbody;
     [SerializeField] private GameObject _visual;
     [SerializeField] private float _movementSpeed;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private LayerMask _whatIsGround;
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private Transform _bulletSpawnPoint;
 
-    // Update is called once per frame
+    private bool _playerFacedRight;
+
+
+
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
+        
+        Movement(horizontal);
+        Animation(horizontal);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 rotation = new Vector3(0, 0, 0);
+            if(!_playerFacedRight)
+                 rotation = new Vector3(0, 0, 180);
+            
+            Instantiate(_bulletPrefab, _bulletSpawnPoint.position , Quaternion.Euler(rotation));
+        }
+    }
+
+
+    public void Movement(float horizontal)
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+          _rigidbody.AddForce(Vector2.up * _jumpForce);  
+        }
+        
+         transform.position += new Vector3(_movementSpeed, 0, 0) * horizontal * Time.deltaTime;
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 0.3f,_whatIsGround);
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.name);
+            return true;
+        }
+
+        return false;
+    }
+    
+    public void Animation(float horizontal)
+    {
         _animator.SetFloat("MovementSpeed", Mathf.Abs(horizontal));
         if (horizontal > 0)
         {
             _visual.transform.localScale = new Vector3(1, 1, 1);
+            _playerFacedRight = true;
         }
         else if (horizontal < 0)
         {
             _visual.transform.localScale = new Vector3(-1, 1, 1);
+            _playerFacedRight = false;
         }
-
-        transform.position += new Vector3(_movementSpeed, 0, 0) * horizontal * Time.deltaTime;
-
     }
+    
 }
